bab7ba9fdc90267a31e1968c59f9ac764536c4e4
Joe Mother <112591263+XeroRel0adz@users.noreply.github.com>
2024-06-28 19:49:53 -0400

    Update Bot-code.py

--- Bot-code.py
+++ Bot-code.py
@@ -1,5 +1,6 @@
 import discord
 import time
+import random
 from discord import app_commands
 from discord.ext import commands
 
@@ -8,8 +9,8 @@ intents = discord.Intents.all()
 intents.messages = True
 intents.guilds = True
 
-
 bot = commands.Bot(command_prefix='/', intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -17,19 +18,39 @@ async def on_ready():
     print('-----')
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send('hello')
+async def online(ctx):
+    await ctx.send('The bot is online and working!')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('@everyone')
+async def helpme(ctx):
+    await ctx.send('''
+Help is here! Heres a list of commands below:
+1. /online 
+2. /helpme
+3. /coinflip #flips a coin and gives a heads or tails response to decide things                  
+4. /rockpaperscissors #play rock, paper, scissors - it's not fully done yet
+5. /
+''')
 
 @bot.command()
-async def someone(ctx):
-    await ctx.send('your mother')
+async def coinflip(ctx):
+    coin = random.randint(1, 2)
+    if coin == 1:
+        await ctx.send("Tails")
+    if coin == 2:
+        await ctx.send("Heads")
+
 
 @bot.command()
-async def test(ctx):
-    await ctx.send('test complete')
+async def rockpaperscissors(ctx):
+    rock = random.randint(1, 2, 3)
+    if rock == 1:
+        await ctx.send("Rock")
+
+    if rock == 2:
+        await ctx.send("Paper")
+
+    if rock == 3:
+        await ctx.send("Scissors")
 
 bot.run('123')
